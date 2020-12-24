328fac174e981d9c5892cdb4dcfcb6c45304a37d
Vitold S <vit1251@gmail.com>
2020-12-25 00:45:26 +0300

    Code style

--- examples/test_sdl.mjs
+++ examples/test_sdl.mjs
@@ -1,11 +1,10 @@
 
 import SDL2 from '../index.mjs';
-import fs from 'fs';
 
-let context = {
-	window : null,
-	rendere : null
-}
+const context = {
+    window : null,
+    rendere : null
+};
 
 function render(context)
 {
@@ -31,6 +30,7 @@ function main() {
 
     const displayIndex = 0;
     const [w, h] = SDL2.SDL_GetDesktopDisplayMode(displayIndex);
+
     console.log(`Display resolution: ${w} x ${h}`);
 
     const [screen_width, screen_height] = [w, h];
@@ -38,60 +38,46 @@ function main() {
     const sdl_window = SDL2.SDL_CreateWindow("SDL Sample", 0, 0, screen_width, screen_height, SDL2.SDL_WINDOW_FULLSCREEN);
     const sdl_renderer = SDL2.SDL_CreateRenderer( sdl_window, -1, SDL2.SDL_RENDERER_ACCELERATED );
 
-    // Initialize renderer color
+    //Initialize renderer color
 
     SDL2.SDL_SetRenderDrawColor(sdl_renderer, [0xFF, 0xFF, 0xFF, 0xFF]);
 
     let quit = false;
 
-	context.window = sdl_window;
-	context.renderer = sdl_renderer;
-	render(context);
-	
-	let cursor = SDL2.SDL_CreateSystemCursor(SDL2.SDL_SYSTEM_CURSOR_ARROW);
-	SDL2.SDL_SetCursor(cursor);
-	SDL2.SDL_ShowCursor(1);
-			
-	while(!quit)
-	{
-		let event = {};
-		SDL2.SDL_PumpEvents();
-		while(1) {
-			let ret = SDL2.SDL_PeepEvents(event, 1, SDL2.SDL_GETEVENT, SDL2.SDL_FIRSTEVENT, SDL2.SDL_LASTEVENT);
-			if(ret == 1) break;
-			SDL2.SDL_Delay(10);
-			render(context);
-			SDL2.SDL_PumpEvents();
-		}
+    context.window = sdl_window;
+    context.renderer = sdl_renderer;
+    render(context);
 
-		switch(event.type)
-		{
-			case "MOUSEBUTTONDOWN":
-				break;
-			case "MOUSEBUTTONUP":
-				break;
-			case "MOUSEWHEEL":
-				break;
-			case "WINDOWEVENT":
-				if(event.event == "WINDOWEVENT_RESIZED") {
-					[screen_width, screen_height] = SDL2.SDL_GetWindowSize(context.window);
+    const cursor = SDL2.SDL_CreateSystemCursor(SDL2.SDL_SYSTEM_CURSOR_ARROW);
+    SDL2.SDL_SetCursor(cursor);
+    SDL2.SDL_ShowCursor(1);
 
-					render(context);
-				} else if(event.event == "WINDOWEVENT_SIZE_CHANGED") {
-		
-				} else if(event.event == "WINDOWEVENT_EXPOSED") {
-				}
-				break;
-			case "KEYDOWN":
-				break;
-			case "QUIT":
-				quit = true;
-				break;
-		}
-	}
-	SDL2.SDL_DestroyRenderer(context.renderer );
-	SDL2.SDL_DestroyWindow(context.window);
-	SDL2.SDL_Quit();
+    while(!quit)
+    {
+        let event = {};
+        SDL2.SDL_PumpEvents();
+        while(1)
+        {
+            const ret = SDL2.SDL_PeepEvents(event, 1, SDL2.SDL_GETEVENT, SDL2.SDL_FIRSTEVENT, SDL2.SDL_LASTEVENT);
+            if(ret == 1)
+                break;
+            SDL2.SDL_Delay(10);
+            render(context);
+            SDL2.SDL_PumpEvents();
+        }
+
+        if (event.type === 'QUIT')
+        {
+            quit = true;
+            break;
+        }
+
+    }
+
+    SDL2.SDL_DestroyRenderer(context.renderer);
+    SDL2.SDL_DestroyWindow(context.window);
+    SDL2.SDL_Quit();
+
 }
 
 main();
